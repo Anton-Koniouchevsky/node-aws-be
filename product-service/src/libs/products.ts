826c1c304672fe5fda31db3b68edef72478be18f
Anton Koniouchevski <Anton_Koniouchevski@epam.com>
2021-04-18 17:13:01 +0300

    associate stocks with products and include in requests

--- product-service/src/libs/products.ts
+++ product-service/src/libs/products.ts
@@ -29,22 +29,54 @@ const ProductModel = sequelize.define('Product', {
   timestamps: false,
 });
 
+const StockModel = sequelize.define('Stock', {
+  product_id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+  },
+  count: {
+    type: DataTypes.INTEGER,
+  }
+}, {
+  tableName: 'stocks',
+  timestamps: false,
+});
+
+ProductModel.hasOne(StockModel, {
+  foreignKey: 'product_id',
+  targetKey: 'id'
+});
+StockModel.belongsTo(ProductModel, {
+  foreignKey: 'product_id',
+  targetKey: 'id'
+});
+
+interface Stock {
+  count: number
+};
 
 interface Product {
   id: string,
   title: string,
   description: string,
   price: number,
+  stock?: Stock,
 };
 
-async function getProducts(): Promise<Product[]> {
-  return ProductModel.findAll();
+function getProducts(): Promise<Product[]> {
+  return ProductModel.findAll({
+    include: {
+      model: StockModel,
+      attributes: ['count']
+    }
+  });
 }
 
-async function getProductById(productId: string): Promise<Product|Error> {
-  return await ProductModel.findAll({
-    where: {
-      id: productId
+function getProductById(productId: string): Promise<Product|Error> {
+  return ProductModel.findByPk(productId, {
+    include: {
+      model: StockModel,
+      attributes: ['count']
     }
   });
 };
